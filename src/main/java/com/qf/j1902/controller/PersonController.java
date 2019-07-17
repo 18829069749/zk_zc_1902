540ef75d2202932de8f722cb1ef6f214e98b2ef8
18829069749 <1658343900@qq.com>
2019-07-17 11:13:06 +0800

    cc

--- src/main/java/com/qf/j1902/controller/PersonController.java
+++ src/main/java/com/qf/j1902/controller/PersonController.java
@@ -1,7 +1,21 @@
 package com.qf.j1902.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+
 /**
  * Created by Administrator on 2019/7/17.
  */
+@Controller
 public class PersonController {
+    @RequestMapping(value="git")
+    public String renz2() {
+
+        return "git";
+    }
+
 }
